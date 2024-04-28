8319333c740a862190308585d05b8767c2d2e50e
Diana Duong <dduong013@gmail.com>
2024-04-28 14:03:36 -0500

    Initial Commit

---
+++ KodeHaus-v2/KodeHaus_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  KodeHaus_v2App.swift
+//  KodeHaus-v2
+//
+//  Created by Diana Duong on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct KodeHaus_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
