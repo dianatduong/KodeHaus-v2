8f993cb375d012a8ab17d4d0ecc815cf3f0a8be1
Diana Duong <dduong013@gmail.com>
2024-07-11 15:42:42 -0700

    add header

--- KodeHaus-v2/KodeHaus_v2App.swift
+++ KodeHaus-v2/KodeHaus_v2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KodeHaus_v2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
--- KodeHaus-v2/LoginView.swift
+++ KodeHaus-v2/LoginView.swift
@@ -11,10 +11,33 @@ struct LoginView: View {
     
     var body: some View {
        
+        //HEADER START
         VStack {
             // Logo
+            AppName()
+            
             //header
+            Text("A community for career changers.")
+                .font(Font.system(size: 20, weight: .light))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 30)
+                .foregroundColor(.gray)
+            
+            //header image
+            Image("loginImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 200)
+                .padding(.leading, 30)
+                .padding(.top, 25)
+            
+            //Image credit
+            Text("All Illustrations by Icons 8 from Ouch!")
+                .font(.system(size: 8))
+                .foregroundColor(.gray)
         }
+        .padding(.top, 50)
+        //HEADER END
         
         VStack {
             // email text field
